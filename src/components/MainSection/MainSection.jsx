3024090b167a5f6b45ad5bf727ba180f83485c2d
MochaFurr <150847830+MochaFurr@users.noreply.github.com>
2023-11-16 12:42:32 +0200

    update
    reposition "Get started" button, for small screens
    
    prepared the Stats section

--- src/components/MainSection/MainSection.jsx
+++ src/components/MainSection/MainSection.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import styles from "../../style";
 import { discount, robot } from "../../assets";
 import GetStarted from "../GetStarted/GetStarted";
@@ -41,15 +40,21 @@ const MainSection = () => {
         </p>
       </div>
 
-      <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
-        <img src={robot} alt="billing" className="w-[100%] h-[100%] relative z-[5]" />
+      <div
+        className={`flex flex-1 ${styles.flexCenter} relative my-10 md:my-0`}
+      >
+        <img
+          src={robot}
+          alt="billing"
+          className="relative z-[5] h-[100%] w-[100%]"
+        />
 
-        <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient" />
-        <div className="absolute z-[1] w-[80%] h-[80%] rounded-full white__gradient bottom-40" />
-        <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient" />
-        <div className={`ss:hidden ${styles.flexCenter}`}>
-          <GetStarted/>
-        </div>
+        <div className="pink__gradient absolute top-0 z-[0] h-[35%] w-[40%]" />
+        <div className="white__gradient absolute bottom-40 z-[1] h-[80%] w-[80%] rounded-full" />
+        <div className="blue__gradient absolute bottom-20 right-20 z-[0] h-[50%] w-[50%]" />
+      </div>
+      <div className={`ss:hidden ${styles.flexCenter}`}>
+        <GetStarted />
       </div>
     </section>
   );
